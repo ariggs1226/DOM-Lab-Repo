7a281a15b200a8e279eff4d612e707aa122a5f49
Aric Riggs <ariggs1226@gmail.com>
2022-02-14 13:09:05 -0600

    Had a crazy sunday and had to spend day fixing internet and then was still unable to connect until this morning and made repo for files I did before

---
+++ app.js
@@ -0,0 +1,13 @@
+let addSqBtn = document.createElement('button').name = 'Add Square';
+let addSqClk = document.addEventListener('click', addDivBtn());
+let addSqHvr = document.addEventListener('hover', showDivID());
+let num = 1;
+
+function addDivBtn() {
+    let div = document.createElement('div').className = "btnDiv d-flex flex-row flex-wrap align-items-start";
+    div.id = num.toString()++;
+};
+
+function showDivID() {
+    div.
+}
