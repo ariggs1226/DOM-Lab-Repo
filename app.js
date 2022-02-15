8fba0223c4753b9cd66ec9db13062ee87d1c8824
Aric Riggs <ariggs1226@gmail.com>
2022-02-15 15:29:49 -0600

    Finished the lab with aid of the walkthrough vid and repo. The 99Lines lab also made it easier to understand.

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
-let addSqBtn = document.createElement('button').name = 'Add Square';
-let addSqClk = document.addEventListener('click', addDivBtn());
-let addSqHvr = document.addEventListener('hover', showDivID());
-let num = 1;
+const addSqBtn = document.createElement('button')
+addSqBtn.textContent = "Add Square";
+document.body.appendChild(addSqBtn);
+
+const squareHolder = document.createElement("div");
+squareHolder.id = 'square-holder';
+document.body.appendChild(squareHolder);
+
+let numSquares = 0;
 
 function addDivBtn() {
-    let div = document.createElement('div').className = "btnDiv d-flex flex-row flex-wrap align-items-start";
-    div.id = num.toString()++;
+    const newDiv = document.createElement('div');
+    newDiv.className = "square d-flex flex-row flex-wrap align-items-start";
+    numSquares++;
+    newDiv.id = numSquares;
+
+    newDiv.addEventListener("mouseover", function () {
+        newDiv.textContent = newDiv.id;
+    });
+
+    newDiv.addEventListener("mouseout", function () {
+        newDiv.textContent = "";
+    });
+
+    newDiv.addEventListener("click", function () {
+        let randomColor = generateRandomColor();
+        newDiv.style.backgroundColor = randomColor;
+    });
+
+    newDiv.addEventListener("dblclick", function () {
+        if (newDiv.id % 2 === 0) {
+            if (newDiv.nextSibling) {
+                newDiv.nextSibling.remove();
+            } else {
+                alert("There is no next sibling!");
+            }
+        } else {
+            if (newDiv.previousSibling) {
+                newDiv.previousSibling.remove();
+            } else {
+                alert("There is no previous sibling!");
+            }
+        }
+    });
+
+    squareHolder.appendChild(newDiv);
 };
 
-function showDivID() {
-    div.
+function generateRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
+addSqBtn.addEventListener('click', addDivBtn);
